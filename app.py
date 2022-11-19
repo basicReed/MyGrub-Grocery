04c3b291fc402f2f5e0127ce3c2ade4a8fa867cb
basicReed <jtreed.itsallgood@gmail.com>
2022-11-18 18:01:09 -0800

    secret key local err fix

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import requests
 import json
 from forms import UserAddForm, LoginForm, EditUserForm
 from models import db, connect_db, User, Groceries, Favorites
-from secret_key import SECRET_KEY, API_KEY_LOCAL
+from secret_key import SECRET_KEY_LOCAL, API_KEY_LOCAL
 
 
 CURR_USER_KEY = "curr_user"
@@ -26,7 +26,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = uri or 'postgresql:///mygrub'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', SECRET_KEY)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', SECRET_KEY_LOCAL)
 
 app.config['SESSION_COOKIE_SAMESITE'] = None
 
